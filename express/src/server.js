44a5e39dd02f01ff02fa9fb1e4fa29645f828d39
oatfazwaz <93711042+oatfazwaz@users.noreply.github.com>
2022-03-21 11:06:23 +0700

    setup eslint

--- express/src/server.js
+++ express/src/server.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+
+const app = express();
 const port = 4000;
-const mongoose = require('mongoose')
 
 app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`);
+  // eslint-disable-next-line no-console
+  console.warn(`Listening at http://localhost:${port}`);
 });
 
-mongoose.connect('mongodb://localhost:27017/node-api-101', { useNewUrlParser: true })
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
-    res.send('hello world')
-})
+  res.send('hello world');
+});
